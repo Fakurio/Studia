c2dfbbf5172a7253430be3461f561d9b16abcac4
Fakurio <93683821+Fakurio@users.noreply.github.com>
2022-05-22 19:48:48 +0200

    Delete Django/website directory

--- Django/website/main/forms.py
+++
@@ -1,7 +0,0 @@
-import imp
-from django import forms
-
-class CreateNewList(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
-    check = forms.BooleanField(required=False)
-
--- Django/website/register/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-
-
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    field_order = ["username","email","password1","password2"]
-    
