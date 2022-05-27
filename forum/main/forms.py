375d52e625423aa88f196c6e81453035deae1a70
Kamil <kamil.zelazowski.2@gmail.com>
2022-05-27 22:51:18 +0200

    Adding create/delete post feature

--- forum/main/forms.py
+++ forum/main/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     field_order = ["username", "email", "password1", "password2"]
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "description"]
--- forum/main/views.py
+++ forum/main/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import PostForm, RegisterForm
 from django.contrib.auth import login, logout, authenticate
-
+from django.contrib.auth.decorators import login_required
+from .models import Post
 # Create your views here.
 
+@login_required(login_url="/login")
 def home(response):
-    return render(response, "main/home.html")
+    posts = Post.objects.all()
+
+    if response.method == "POST":
+        post_id = response.POST.get("post-id")
+        post = Post.objects.get(id=post_id)
+        if post and post.author == response.user:
+            post.delete()
+            
+    return render(response, "main/home.html", {"posts": posts})
+
+@login_required(login_url="/login")
+def create_post(response):
+    if response.method == "POST":
+        form = PostForm(response.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = response.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+
+    return render(response, "main/create_post.html", {"form": form})
 
 def sign_up(response):
     if response.method == "POST":
